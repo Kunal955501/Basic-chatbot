f0dee2d25347faa6189353bb674945e507fc751d
Kunal955501 <kunal73797379@gmail.com>
2025-07-13 16:25:05 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+def chatbot():
+    print("🤖 Welcome to CodeAlpha Chatbot!")
+    print("Type 'bye' to exit.\n")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Bot: Hi!")
+        elif user_input == "how are you":
+            print("Bot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Bot: Goodbye! 👋")
+            break
+        else:
+            print("Bot: Sorry, I don't understand that.")
+
+# Run the chatbot
+chatbot()
